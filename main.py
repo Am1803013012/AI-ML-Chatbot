deeaa4a3d267246dc9ccc92a8ef6e78fcc66fa9a
Aayush Kumar <61198949+Aayush-eng@users.noreply.github.com>
2021-10-01 12:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-#import cv
-print("Hello")
 
-print("__name__ value: ", __name__)
+
+
+from time import sleep
+
+print("This is my file to demonstrate best practices.")
+
+def process_data(data):
+    print("Beginning data processing...")
+    modified_data = data + " that has been modified"
+    sleep(3)
+    print("Data processing finished.")
+    return modified_data
 
 def main():
+    data = "My data read from the Web"
+    print(data)
+    modified_data = process_data(data)
+    print(modified_data)
 
-    print("python main function")
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     main()
+
+
+
+    
+
+
+
+ 
